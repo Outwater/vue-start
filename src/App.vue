71bb5d51d8e4c1452618f23848f195835d99934a
Insoo-Seo <kocon135@gmail.com>
2022-05-16 18:27:35 +0900

    init(vue-start) vue 강의를 적용해보기 위한 repo 생성

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <Hello />
+</template>
+
+<script>
+import Hello from "@src/components/Hello";
+
+export default {
+  components: {
+    Hello,
+  },
+  data() {
+    return {
+      msg: "Hello Vue!!!~?~?!1!",
+    };
+  },
+};
+</script>
+
+//
+<style lang="css">
+h1 {
+  color: red;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import * as Vue from "vue";
+import App from "./App.vue";
+import bar from "./bar.js";
+
+Vue.createApp(App).mount("#app");
+
+bar();
