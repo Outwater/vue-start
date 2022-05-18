ca868d57af5d830ffea2ece68ae4194fb01e77b3
Insoo-Seo <kocon135@gmail.com>
2022-05-18 12:20:03 +0900

    study(mixin): 기본적인 mixin 정의 및 사용 실습

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
-  <div>
-    {{ msg }}
-  </div>
+  <h1>{{ msg }}</h1>
+  <h1>{{ count + 1 }}</h1>
+
+  <Hello />
 </template>
 
 <script>
+  import Hello from "@/components/HelloItem";
+  import sampleMixin from "@/mixins/sample";
+
   export default {
+    components: {
+      Hello,
+    },
+    mixins: [sampleMixin],
     data() {
       return {
         msg: "hello vue!",
       };
     },
-    created() {
-      this.$myName("https://jsonplaceholder.typicode.com/todos/1").then((res) =>
-        console.log(res)
-      );
-    },
+    created() {},
   };
 </script>
--- src/components/HelloItem.vue
+++ src/components/HelloItem.vue
@@ -1,20 +1,24 @@
 <template>
-  <h1>Hello</h1>
+  <h1>{{ msg }}</h1>
+  <h1>{{ count }}</h1>
 </template>
 
 <script>
-export default {
-  created() {
-    console.log("Hello 생성");
-  },
-  unmounted() {
-    console.log("Hello 삭제");
-  },
-};
+  import sampleMixin from "@/mixins/sample";
+
+  export default {
+    mixins: [sampleMixin],
+    created() {
+      console.log("Hello 생성");
+    },
+    unmounted() {
+      console.log("Hello 삭제");
+    },
+  };
 </script>
 <style scoped lang="scss">
-$color: red;
-h1 {
-  color: $color;
-}
+  $color: red;
+  h1 {
+    color: $color;
+  }
 </style>
